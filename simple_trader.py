175b8b4fdb3bf30b22ef5865859e7b37b1e1e04f
thammo4 <thammo4@uic.edu>
2024-08-20 12:45:25 -0400

    Update simple trader to track PnL and drawdown

--- simple_trader.py
+++ simple_trader.py
@@ -1,8 +1,9 @@
 # FILE: `StockTrader/simple_trader.py`
 from config import * # imports all necessary modules and variables not explicitly defined
 
-stock_universe = ['NVR', 'COIN', 'LMT', 'LLY', 'ERIC', 'C', 'ZBH', 'AJG', 'SPOT', 'IMO'];
+stock_universe = ['KMI', 'COIN', 'LMT', 'LLY', 'SHEL', 'C', 'WMB', 'IMO', 'LYB', 'MFA', 'MSM', 'MSCI', 'MDT', 'MCK', 'AES', 'MTN', 'VOC', 'URI', 'USAC', 'CEFD', 'USPH', 'TEN', 'TROX', 'TD', 'TTC', 'THO', 'BK', 'TT'];
 pnl = 0.0;
+drawdown = 0.0;
 trade_count = 0;
 trades = [];
 
@@ -18,45 +19,66 @@ def market_hours ():
 
 
 def buy (symbol, stock_price):
-	global pnl, trade_count;
+	print(f'****** BUY [{datetime.now().strftime("%H:%M:%S")}]******');
+	global pnl, drawdown, trade_count;
+	qty_to_buy = random.randint(1,100);
 	acct_balance = acct.get_account_balance().iloc[0]['total_cash'];
-
-	if acct_balance > stock_price:
-		print(f'Trying: Buy {symbol} at ${stock_price}....Cash: ${acct_balance}');
-		order = equity_order.order(symbol=symbol, side='buy', quantity=1, order_type='market', duration='day');
+	if acct_balance > stock_price*qty_to_buy:
+		print(f'Trying: Buy {qty_to_buy} {symbol} at ${stock_price}....Cash: ${acct_balance}');
+		order = equity_order.order(symbol=symbol, side='buy', quantity=qty_to_buy, order_type='market', duration='day');
 		outcome = list(order.keys())[0];
 		if outcome == 'order':
 			print(f'Buy Order Placed: {symbol}');
 			trade_count += 1;
-			trades.append({'symbol':symbol, 'buy_price':stock_price, 'sell_price':None});
+			pnl -= stock_price*qty_to_buy;
+			drawdown += stock_price * qty_to_buy;
+			# print(f'Drawdown: [{symbol}]: ${drawdown:.3f}');
+			trades.append({'symbol':symbol, 'buy_price':stock_price, 'buy_shares':qty_to_buy, 'sell_price':None, 'sell_shares':None});
 		elif outcome == 'errors':
 			print(f'Buy Order ERROR: {symbol}');
 	else:
 		print(f'Insufficient Funds [{symbol}] \tCash: ${acct_balance}....Price: ${stock_price}');
+		print('Selling for $$$$$');
+		new_symbol = random.choice(list(acct.get_positions()['symbol']));
+		new_price = quotes.get_quote_day(new_symbol, True);
+		sell(
+			symbol=new_symbol,
+			stock_price=new_price
+		);
 
 def sell (symbol, stock_price):
-	global pnl, trade_count;
+	print(f'****** SELL [{datetime.now().strftime("%H:%M:%S")}] ******');
+	global pnl, drawdown, trade_count;
 	current_positions = acct.get_positions();
 	portfolio_symbols = list(current_positions['symbol']);
 
 	if symbol in portfolio_symbols:
 		pos = current_positions.loc[current_positions['symbol'] == symbol].iloc[0];
 		qty_owned = pos['quantity'];
+		if qty_owned == 0:
+			print(f'Zero quantity: {symbol}');
+			return;
+
+		qty_to_sell = random.randint(1, int(qty_owned));
+
 		sale_proceeds = pos['quantity'] * stock_price;
 		cost_basis = pos['cost_basis'];
 
-		pnl_individual = sale_proceeds - cost_basis;
-		pnl += pnl_individual;
-		print(f'Trying: Sell {qty_owned} {symbol} for ${sale_proceeds:.3f}');
-		order = equity_order.order(symbol=symbol, side='sell', quantity=qty_owned, order_type='market', duration='day');
+		# pnl_individual = sale_proceeds - cost_basis;
+		# pnl += pnl_individual;
+		drawdown -= sale_proceeds;
+		pnl += sale_proceeds - cost_basis;
+		print(f'Trying: Sell {qty_to_sell} {symbol} for ${sale_proceeds:.3f}');
+		order = equity_order.order(symbol=symbol, side='sell', quantity=qty_to_sell, order_type='market', duration='day');
 		outcome = list(order.keys())[0];
 		if outcome == 'order':
 			print(f'Sell Order Placed: {symbol}');
-			print(f'PnL [{symbol}]: ${pnl:.3f}');
+			# print(f'PnL [{symbol}]: ${pnl:.3f}');
 			trade_count +=1;
 			for t in trades:
 				if t['symbol'] == symbol and t['sell_price'] is None:
 					t['sell_price'] = stock_price;
+					t['sell_shares'] = qty_to_sell;
 					break;
 		elif outcome == 'errors':
 			print(f'Sell Order ERROR: {symbol}');
@@ -65,36 +87,53 @@ def sell (symbol, stock_price):
 
 
 def trade_algo():
-	global pnl, trade_count;
+	global pnl, drawdown, trade_count;
+	iter = 0;
 	while market_hours():
-		symbol = random.choice(stock_universe);
-		stock_price = quotes.get_quote_day(symbol).iloc[0]['last'];
+		buy_symbol = random.choice(stock_universe);
+		stock_price = quotes.get_quote_day(buy_symbol).iloc[0]['last'];
 
-		buy(symbol, stock_price);
-		print(97*'-', '\n');
-		time.sleep(300);
+		buy(buy_symbol, stock_price);
+		# print(97*'-', '\n');
+		print('-------')
+		# time.sleep(.00075);
 
-		sell(symbol, stock_price);
-		print(97*'-', '\n');
+		sell_symbol = random.choice(stock_universe);
+		stock_price = quotes.get_quote_day(sell_symbol).iloc[0]['last'];
 
-		print(f"****** REPORT ******");
-		print(f"Symbol: {symbol}");
-		print(f"PnL: ${pnl:.3f}");
-		print(f"Trade Count: {trade_count}");
-		print(97*'-', '\n\n\n');
-		time.sleep(300);
+		sell(sell_symbol, stock_price);
+		# print(97*'-', '\n');
+		print('-------')
+
+		print(f"****** REPORT [{datetime.now().strftime('%H:%M:%S')}] ******");
+		# print(f"Symbol: {symbol}");
+		# print(f"Buy Symbol: {buy_symbol} ..... Sell Symbol: {sell_symbol}")
+		print(f"(Buy, Sell) = ({buy_symbol}, {sell_symbol})");
+		if iter % 15 == 0:
+			print(f"Drawdown: ${drawdown:.3f}");
+			print(f"PnL: ${pnl:.3f}");
+			print(f"Trade Count: {trade_count}");
+			print(f"Transactions:");
+			for t in trades:
+				print(f"Symbol: {t['symbol']} | Bought: ${t['buy_price']} ({t['buy_shares']}) | Sold: ${t['sell_price']} ({t['sell_shares']})");
+			print(" ############## END OF BUY-SELL SEQUENCE ############## ");
+			print(97*'-', '\n\n\n');
+			time.sleep(1.5);
+		iter += 1;
+		# print(" ############## END OF BUY-SELL SEQUENCE ############## ");
 
 	print(f"!!!!!!!!! REPORT - END OF DAY !!!!!!!!!");
 	print(f"Total PnL: ${pnl:.3f}");
 	print(f"Total Trades: {trade_count}");
 	print(f"Trades:");
 	for t in trades:
-		print(f"Symbol: {t['symbol']} | Bought: ${t['buy_price']} | Sold: ${t['sell_price']}");
-	print('\n -------------- END OF TRADING --------------\n');
+		print(f"Symbol: {t['symbol']} | Bought: ${t['buy_price']} ({t['buy_shares']}) | Sold: ${t['sell_price']} ({t['sell_shares']})");
+	print('\n -------------- END OF TRADING --------------\n\n\n\n');
 
 
 if __name__ == '__main__':
 	print(f"Market Open? -> {market_hours()}");
+	print('\n\n\n');
 	trade_algo();
 	if market_hours():
 		trade_algo();
@@ -103,6 +142,92 @@ if __name__ == '__main__':
 
 
 
+
+
+#
+# SAMPLE OUTPUT
+#
+
+# ****** BUY [11:35:30]******
+# Trying: Buy 3 LMT at $553.81....Cash: $339791.36
+# Buy Order Placed: LMT
+# -------
+# ****** SELL [11:35:33] ******
+# Trying: Sell 3.0 SHEL for $215.475
+# Sell Order Placed: SHEL
+# -------
+# ****** REPORT [11:35:33] ******
+# (Buy, Sell) = (LMT, SHEL)
+# Drawdown: $1445.955
+# PnL: $-0.045
+# Trade Count: 2
+# Transactions:
+# Symbol: LMT | Bought: $553.81 (3) | Sold: $None (None)
+#  ############## END OF BUY-SELL SEQUENCE ############## 
+# ------------------------------------------------------------------------------------------------- 
+
+
+
+# ****** BUY [11:35:36]******
+# Trying: Buy 2 LMT at $553.81....Cash: $339791.36
+# Buy Order Placed: LMT
+# -------
+# ****** SELL [11:35:39] ******
+# Stock not currently owned. Skipping. [SHEL]
+# -------
+# ****** REPORT [11:35:39] ******
+# (Buy, Sell) = (LMT, SHEL)
+# Drawdown: $2553.575
+# PnL: $-0.045
+# Trade Count: 3
+# Transactions:
+# Symbol: LMT | Bought: $553.81 (3) | Sold: $None (None)
+# Symbol: LMT | Bought: $553.81 (2) | Sold: $None (None)
+#  ############## END OF BUY-SELL SEQUENCE ############## 
+# ------------------------------------------------------------------------------------------------- 
+
+
+
+# ****** BUY [11:35:41]******
+# Trying: Buy 1 COIN at $196.37....Cash: $337235.45
+# Buy Order Placed: COIN
+# -------
+# ****** SELL [11:35:44] ******
+# Stock not currently owned. Skipping. [WMB]
+# -------
+# ****** REPORT [11:35:44] ******
+# (Buy, Sell) = (COIN, WMB)
+# Drawdown: $2749.945
+# PnL: $-0.045
+# Trade Count: 4
+# Transactions:
+# Symbol: LMT | Bought: $553.81 (3) | Sold: $None (None)
+# Symbol: LMT | Bought: $553.81 (2) | Sold: $None (None)
+# Symbol: COIN | Bought: $196.37 (1) | Sold: $None (None)
+#  ############## END OF BUY-SELL SEQUENCE ############## 
+# ------------------------------------------------------------------------------------------------- 
+
+
+
+# ****** BUY [11:35:47]******
+# Trying: Buy 3 C at $61.29....Cash: $337235.45
+# Buy Order Placed: C
+# -------
+# ****** SELL [11:35:50] ******
+# Stock not currently owned. Skipping. [WMB]
+# -------
+# ****** REPORT [11:35:50] ******
+# (Buy, Sell) = (C, WMB)
+# Drawdown: $2933.815
+# PnL: $-0.045
+# Trade Count: 5
+# Transactions:
+# Symbol: LMT | Bought: $553.81 (3) | Sold: $None (None)
+# Symbol: LMT | Bought: $553.81 (2) | Sold: $None (None)
+# Symbol: COIN | Bought: $196.37 (1) | Sold: $None (None)
+# Symbol: C | Bought: $61.29 (3) | Sold: $None (None)
+#  ############## END OF BUY-SELL SEQUENCE ############## 
+# ------------------------------------------------------------------------------------------------- 
 
 
 
