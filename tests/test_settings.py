2b6efb9ee1619342c4b3baf1cb9b3feab621abfa
thammo4 <thammo4@uic.edu>
2025-02-22 18:11:05 -0500

    Update toml pythontests directive and rewrite settings test from unittest to pytest

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,62 +1,51 @@
+#
+# FILE: `StockTrader/tests/test_settings.py` (test: `StockTrader/src/settings.py`)
+# 	• Confirm that directory paths are correctly specified.
+# 	• Confirm that the dates are correctly defined.
+#
+
 import os
 import logging
-import unittest
+import importlib
+import pytest
 from datetime import datetime, timedelta
 from unittest.mock import patch
-import importlib
-# from StockTrader.settings import(
-# 	STOCK_TRADER_HOME,
-# 	STOCK_TRADER_MARKET_DATA,
-# 	STOCK_TRADER_LOG,
-# 	LOG_FILE,
-# 	logger,
-# 	today,
-# 	today_last_year,
-# 	today_six_months_ago,
-# 	today_three_months_ago,
-# 	today_last_month
-# )
 
-class TestSettings (unittest.TestCase):
+@pytest.fixture
+def settings ():
+	import StockTrader.settings
+	importlib.reload(StockTrader.settings)
+	return StockTrader.settings
 
-	@patch('logging.basicConfig')
-	@patch.dict(os.environ, {"STOCK_TRADER_HOME":"/mock/home", "STOCK_TRADER_MARKET_DATA":"/mock/data", "STOCK_TRADER_LOG":"/mock/logs"})
-	def test_home_path(self, mock_logging):
-		import StockTrader.settings
-		importlib.reload(StockTrader.settings)
-		from StockTrader.settings import (STOCK_TRADER_HOME, STOCK_TRADER_MARKET_DATA, STOCK_TRADER_LOG)
-		self.assertEqual(STOCK_TRADER_HOME, "/mock/home")
-		self.assertEqual(STOCK_TRADER_MARKET_DATA, "/mock/data")
-		self.assertEqual(STOCK_TRADER_LOG, "/mock/logs")
+@pytest.fixture(autouse=True)
+def mock_env ():
+	with patch.dict(os.environ, {"STOCK_TRADER_HOME":"/mock/home", "STOCK_TRADER_MARKET_DATA":"/mock/data", "STOCK_TRADER_LOG":"/mock/logs"}):
+		yield
 
-		mock_logging.assert_called()
-
-
-	def test_dates (self):
-		import StockTrader.settings
-		importlib.reload(StockTrader.settings)
+def test_paths ():
+	'''Test that directory paths are correctly specified'''
+	with patch('logging.basicConfig') as mock_log:
 		from StockTrader.settings import (
-			today,
-			today_last_year,
-			today_six_months_ago,
-			today_three_months_ago,
-			today_last_month
+			STOCK_TRADER_HOME,
+			STOCK_TRADER_MARKET_DATA,
+			STOCK_TRADER_LOG
 		)
+		assert STOCK_TRADER_HOME == "/mock/home"
+		assert STOCK_TRADER_MARKET_DATA == "/mock/data"
+		assert STOCK_TRADER_LOG == "/mock/logs"
+		mock_log.assert_called()
 
-		dt = datetime.today()
-		today_expected = dt.strftime("%Y-%m-%d")
-		last_year_expected = (dt-timedelta(weeks=52)).strftime("%Y-%m-%d")
-		six_months_ago_expected = (dt-timedelta(weeks=26)).strftime("%Y-%m-%d")
-		three_months_ago_expected = (dt-timedelta(weeks=13)).strftime("%Y-%m-%d")
-		last_month_expected = (dt-timedelta(weeks=4)).strftime("%Y-%m-%d")
+def test_dates (settings):
+	'''Test that dates are correctly defined'''
+	dt = datetime.today()
+	today_expected = dt.strftime('%Y-%m-%d')
+	last_year_expected = (dt-timedelta(weeks=52)).strftime("%Y-%m-%d")
+	six_months_ago_expected = (dt-timedelta(weeks=26)).strftime("%Y-%m-%d")
+	three_months_ago_expected = (dt-timedelta(weeks=13)).strftime("%Y-%m-%d")
+	last_month_expected = (dt-timedelta(weeks=4)).strftime("%Y-%m-%d")
 
-		self.assertEqual(today, today_expected)
-		self.assertEqual(today_last_year, last_year_expected)
-		self.assertEqual(today_six_months_ago, six_months_ago_expected)
-		self.assertEqual(today_three_months_ago, three_months_ago_expected)
-		self.assertEqual(today_last_month, last_month_expected)
-
-
-
-if __name__ == '__main__':
-	unittest.main()
+	assert settings.today == today_expected
+	assert settings.today_last_year == last_year_expected
+	assert settings.today_six_months_ago == six_months_ago_expected
+	assert settings.today_three_months_ago == three_months_ago_expected
+	assert settings.today_last_month == last_month_expected
