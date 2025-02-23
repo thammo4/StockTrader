0fdeaef7f30f414db26a65cfc10bf94e459077ad
thammo4 <thammo4@uic.edu>
2025-02-23 15:35:01 -0500

    feat: add module and unit test that pre-defines tradier client library variables for account, market data, order exec

---
+++ src/StockTrader/tradier.py
@@ -0,0 +1,37 @@
+#
+# FILE: `StockTrader/src/tradier.py`
+# Instantiates classes from uvatradier Tradier API Client Library
+#
+
+import os
+import dotenv
+from uvatradier import Account, Quotes, EquityOrder, OptionsOrder, OptionsData, Stream
+from StockTrader.settings import STOCK_TRADER_HOME
+
+
+#
+# Define API Keys Using Local .env File
+#
+
+env_path = os.path.join(os.path.join(STOCK_TRADER_HOME, ".env"))
+dotenv.load_dotenv(env_path)
+
+tradier_acct = os.getenv('tradier_acct')
+tradier_token = os.getenv('tradier_token')
+
+tradier_acct_live = os.getenv('tradier_acct_live')
+tradier_token_live = os.getenv('tradier_token_live')
+
+acct = Account(tradier_acct, tradier_token)
+quotes = Quotes(tradier_acct, tradier_token)
+equity_order = EquityOrder(tradier_acct, tradier_token)
+options_order = OptionsOrder(tradier_acct, tradier_token)
+options_data = OptionsData(tradier_acct, tradier_token)
+
+
+acctL = Account(tradier_acct_live, tradier_token_live, True)
+quotesL = Quotes(tradier_acct_live, tradier_token_live, True)
+equity_orderL = EquityOrder(tradier_acct_live, tradier_token_live, True)
+options_orderL = OptionsOrder(tradier_acct_live, tradier_token_live, True)
+options_dataL = OptionsData(tradier_acct_live, tradier_token_live, True)
+streamL = Stream(tradier_acct_live, tradier_token_live, True)
---
+++ tests/test_tradier.py
@@ -0,0 +1,42 @@
+#
+# FILE: `StockTrader/tests/test_tradier.py`
+#
+
+import os
+import importlib
+import pytest
+from unittest.mock import patch
+
+@pytest.fixture(autouse=True)
+def mock_env():
+	with patch.dict(os.environ, {
+		"tradier_acct":"mock_acct",
+		"tradier_token":"mock_token",
+		"tradier_acct_live":"mock_acct_live",
+		"tradier_token_live":"mock_token_live",
+		# "STOCK_TRADER_HOME":"/mock/home"
+	}):
+		yield
+
+def test_tradier_import ():
+	'''Test that uvatradier Tradier Client Library variables ok'''
+	from StockTrader import tradier
+	importlib.reload(tradier)
+
+	assert tradier.tradier_acct == "mock_acct"
+	assert tradier.tradier_token == "mock_token"
+	assert tradier.tradier_acct_live == "mock_acct_live"
+	assert tradier.tradier_token_live == "mock_token_live"
+
+	assert tradier.acct is not None
+	assert tradier.quotes is not None
+	assert tradier.equity_order is not None
+	assert tradier.options_order is not None
+	assert tradier.options_data is not None
+
+	assert tradier.acctL is not None
+	assert tradier.quotesL is not None
+	assert tradier.equity_orderL is not None
+	assert tradier.options_orderL is not None
+	assert tradier.options_dataL is not None
+	assert tradier.streamL is not None
