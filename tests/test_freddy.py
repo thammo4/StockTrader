a7444736084eda40b11830dd8970072c1965f721
thammo4 <thammo4@uic.edu>
2025-02-24 16:23:41 -0500

    fix: black got me again

--- tests/test_freddy.py
+++ tests/test_freddy.py
@@ -7,15 +7,18 @@ import importlib
 import pytest
 from unittest.mock import patch
 
+
 @pytest.fixture(autouse=True)
-def mock_env ():
-	with patch.dict(os.environ, {"fred_api_key":"mock_fred_api_key"}):
-		yield
+def mock_env():
+    with patch.dict(os.environ, {"fred_api_key": "mock_fred_api_key"}):
+        yield
 
-def test_fred_import ():
-	'''Test that FRED API client instantiation ok'''
-	from StockTrader import freddy
-	importlib.reload(freddy)
 
-	assert freddy.fred_api_key == "mock_fred_api_key"
-	assert freddy.fred is not None
+def test_fred_import():
+    """Test that FRED API client instantiation ok"""
+    from StockTrader import freddy
+
+    importlib.reload(freddy)
+
+    assert freddy.fred_api_key == "mock_fred_api_key"
+    assert freddy.fred is not None
