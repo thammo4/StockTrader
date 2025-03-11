4feabb5b34a8459672d1af71941b4cd6ef353659
thammo4 <thammo4@uic.edu>
2025-03-10 17:00:21 -0700

    fix: app logging format

--- src/StockTrader/settings.py
+++ src/StockTrader/settings.py
@@ -28,7 +28,7 @@ logging.basicConfig(
     filename=LOG_FILE,
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(message)s",
-    datefmt="%Y-%m-%d %H-%M-%S",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 logger = logging.getLogger(__name__)
