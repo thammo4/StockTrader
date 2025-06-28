5afb71597fa7506be77c4f3888edf3d8d018ed81
thammo4 <thammo4@uic.edu>
2025-06-27 22:02:33 -0400

    rewrite tradier options chain staging model to handle schema drift among various symbols

--- dbt/models/staging/tradier/stg_tradier__options.sql
+++ dbt/models/staging/tradier/stg_tradier__options.sql
@@ -1,42 +1,70 @@
 --
 -- FILE: `StockTrader/dbt/models/staging/tradier/stg_tradier__options.sql`
 --
-
 with source as (
-	select * from {{ source('tradier_raw', 'options_af') }}
+	select
+		symbol,
+		last,
+		change,
+		volume,
+		open,
+		high,
+		low,
+		bid,
+		ask,
+		strike,
+		change_percentage,
+		last_volume,
+		trade_date,
+		prevclose,
+		bidsize,
+		bidexch,
+		bid_date,
+		asksize,
+		askexch,
+		ask_date,
+		open_interest,
+		option_type,
+		created_date
+	-- from {{ source('tradier_raw', 'options_af') }}
+	from read_parquet("../data/warehouse/options_af/*.parquet", union_by_name=true)
+),
+parsed_occ as (
+	select
+		*,
+		regexp_extract(symbol, '(^[A-Z]+)', 1) as underlying,
+		regexp_extract(symbol, '^[A-Z]{1,6}\d?([0-9]{6})', 1) as expiry_yymmdd
+	from source
+),
+with_expiry_date as (
+	select
+		*,
+		cast(
+			'20' ||
+			substr(expiry_yymmdd,1,2)
+			|| '-' ||
+			substr(expiry_yymmdd,3,2)
+			|| '-' ||
+			substr(expiry_yymmdd,5,2)
+			as date
+		) as expiry_date
+	from parsed_occ
 )
 
 select
 	-- Contract/ID
-	regexp_extract(symbol, '(^[A-Z]+)', 1) as underlying,
+	underlying,
 	symbol as occ,
-	cast(
-		'20' || substr(regexp_extract(symbol, '^[A-Z]+([0-9]{6})', 1), 1, 2)
-		|| '-' ||
-		substr(regexp_extract(symbol, '^[A-Z]+([0-9]{6})',1),3,2)
-		|| '-' ||
-		substr(regexp_extract(symbol, '^[A-Z]+([0-9]{6})',1),5,2)
-		as date
-	) as expiry_date,
+	expiry_date,
 	option_type,
 	strike as strike_price,
 
-	-- Market Data/TS
-	bidexch as bid_exchange,
-	askexch as ask_exchange,
+	-- Market Data
 	bid as bid_price,
 	ask as ask_price,
 	round((bid+ask)/2.0, 2) as mid_price,
-	last as last_price,
-	change as change_price,
-	change_percentage as change_price_pct,
-	prevclose as prev_close_price,
-
-	-- Timestamps
-	to_timestamp(trade_date/1000) as trade_date,
-	to_timestamp(bid_date/1000) as bid_date,
-	to_timestamp(ask_date/1000) as ask_date,
-	created_date::date as created_date,
+	bidexch as bid_exchange,
+	askexch as ask_exchange,
 
 	-- Trading Activity
 	volume,
@@ -49,12 +77,21 @@ select
 	open as open_price,
 	high as high_price,
 	low as low_price,
-	close as close_price,
+	last as close_price,
+	change as change_price,
+	change_percentage as change_price_pct,
+	prevclose as prev_close_price,
+
+	-- Timestamps
+	to_timestamp(trade_date/1000) as trade_date,
+	to_timestamp(bid_date/1000) as bid_date,
+	to_timestamp(ask_date/1000) as ask_date,
+	cast(created_date as date) as created_date,
 
 	-- Data Quality Indicators
 	case
-		when bid is not null and ask is not null and bid > 0 and ask > 0 and ask > bid
+		when bid is not null and ask is not null and bid > 0 and ask > 0 and ask-bid > 0
 		then true
 		else false
 	end as is_valid_price
-from source
+from with_expiry_date
