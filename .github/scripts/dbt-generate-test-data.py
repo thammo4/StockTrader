ca549b909d48f1f60e3d2089bfef188e5c778578
thammo4 <thammo4@uic.edu>
2025-09-21 15:09:43 -0400

    cleanup stray code in previous commit for dbt generate data script

--- .github/scripts/dbt-generate-test-data.py
+++ .github/scripts/dbt-generate-test-data.py
@@ -20,28 +20,6 @@ def create_dividends_test_data():
 	pass
 
 if __name__ == "__main__":
-	print("-"*60#
-# FILE: `StockTrader/.github/scripts/dbt-generate-sample-data.py`
-#
-
-import os
-import pandas as pd
-import numpy as np
-from datetime import datetime, timedelta
-
-def create_fred_test_data():
-	pass
-
-def create_options_test_data():
-	pass
-
-def create_quotes_test_data():
-	pass
-
-def create_dividends_test_data():
-	pass
-
-if __name__ == "__main__":
 	print("-"*60)
 	print("Generating test data for dbt CI Pipeline")
 	print("-"*60)
