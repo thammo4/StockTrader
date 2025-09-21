0b91a72bc9db6e2359be4aee3f8cd4296759c2fd
thammo4 <thammo4@uic.edu>
2025-09-21 15:41:00 -0400

    implement create_fred_test_data dbt ci function

--- .github/scripts/dbt-generate-test-data.py
+++ .github/scripts/dbt-generate-test-data.py
@@ -3,21 +3,61 @@
 #
 
 import os
+import calendar
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
 
 def create_fred_test_data():
-	pass
+	"""Generate FED interest rate data for TB3MS series"""
+	print("Generating FRED interest rate data...")
+
+	dates = []
+	rates = []
+	start_year = 2025
+	base_rate = 4.5
+
+	for m in range(1,13):
+		last_day_of_month = calendar.monthrange(start_year, m)[1]
+		fred_date = datetime(start_year, m, last_day_of_month)
+		seasonality = 0.2*np.sin(m*np.pi/6)
+		trend = -0.05*(m/12)
+		epsilon = np.random.normal(0,0.15)
+
+		rate = max(0.1, base_rate+seasonality+trend+epsilon)
+
+		# dates.append(fred_date, strftime("%Y-%m-%d"))
+		dates.append(fred_date.strftime("%Y-%m-%d"))
+		rates.append(round(rate,2))
+
+	df_fred = pd.DataFrame({
+		"fred_date": dates,
+		"fred_rate": rates,
+		"created_date": datetime.now().strftime("%Y-%m-%d")
+	})
+
+	output_dir = os.path.join(os.environ["STOCK_TRADER_DWH"], "fred_af")
+	os.makedirs(output_dir, exist_ok=True)
+	output_path = os.path.join(output_dir, "TB3MS.parquet")
+	df_fred.to_parquet(output_path, index=False, engine="pyarrow")
+
+	print(f"Generated {len(df_fred)} FRED records -> {output_path}")
+	print(f"Date Range: {dates[0]}...{dates[-1]}")
+	print(f"Sample Dates: {dates[:3]}...{dates[-3:]}")
+
+	return len(df_fred)
 
 def create_options_test_data():
-	pass
+	print("Generating options test data...")
+	return 0
 
 def create_quotes_test_data():
-	pass
+	print("Generating quotes test data...")
+	return 0
 
 def create_dividends_test_data():
-	pass
+	print("Generating dividends test data...")
+	return 0
 
 if __name__ == "__main__":
 	print("-"*60)
