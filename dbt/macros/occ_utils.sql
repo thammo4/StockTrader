33c7500f71d822838ba9891856d0b18e56618b61
tom hammons <thammo4@uic.edu>
2025-11-04 11:28:14 -0500

    fix occ_parse_underlying dbt macro to capture non-standard root symbols with numeric character in underlying

--- dbt/macros/occ_utils.sql
+++ dbt/macros/occ_utils.sql
@@ -6,19 +6,15 @@
 --
 -- occ_parse_underlying
 -- Extract underlying root symbol given an OCC symbol
--- Accounts for conventional ticker symbols and (ticker symbol + adjustment digit)
--- EX:
--- 		• AAPL240118C00190000 -> AAPL
--- 		• MSFT250321P00250000 -> MSFT
--- 		• CVX1270115P00100000 -> CVX
+-- EX: AAPL240118C00190000, MSFT250321P00250000
 --
 
 {% macro occ_parse_underlying(occ) -%}
-case
-	when regexp_matches(upper(trim({{ occ }})),
-		'^([A-Z]{1,6})\d?(\d{2})(\d{2})(\d{2})([CP])(\d{8})$')
-	then regexp_extract(upper(trim({{ occ }})),
-		'^([A-Z]{1,6})\d?(\d{2})(\d{2})(\d{2})([CP])(\d{8})$', 1)
-	else null
-end
+	case
+		when regexp_matches(upper(trim({{ occ }})),
+			'^([A-Z0-9]{1,6})(\d{2})(\d{2})(\d{2})([CP])(\d{8})$')
+		then regexp_extract(upper(trim({{ occ }})),
+			'^([A-Z0-9]{1,6})(\d{2})(\d{2})(\d{2})([CP])(\d{8})$', 1)
+		else null
+	end
 {%- endmacro %}
