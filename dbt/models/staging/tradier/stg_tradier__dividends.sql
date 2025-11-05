a52d45fcdbb6e2d2c6e46126f2b119c4a26abe85
tom hammons <thammo4@uic.edu>
2025-11-05 10:45:45 -0500

    add dquality field to stg_tradier__dividends to distinguish regular/special dividends

--- dbt/models/staging/tradier/stg_tradier__dividends.sql
+++ dbt/models/staging/tradier/stg_tradier__dividends.sql
@@ -10,6 +10,16 @@ select
 	symbol,
 	round(cash_amount,2) as cash_amount,
 	ex_date::date as ex_dividend_date,
-	frequency
+	frequency,
+	case
+		when frequency = 0 then 'special'
+		when frequency = 1 then 'annual'
+		when frequency = 2 then 'semi_annual'
+		when frequency = 4 then 'quarterly'
+		when frequency = 12 then 'monthly'
+		else 'unknown'
+	end as dividend_type,
+	frequency > 0 and frequency < 13 as is_regular_dividend,
+	created_date::date as created_date
 from source
-order by ex_dividend_date
+order by symbol, ex_dividend_date
