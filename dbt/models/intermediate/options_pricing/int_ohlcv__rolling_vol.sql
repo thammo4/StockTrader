dcc7c71de71df4409432bf786ce3b79baf6cd157
tom hammons <thammo4@uic.edu>
2025-12-24 23:17:59 -0500

    refactor int_ohlcv_rolling_vol to include dedup CTE and prepare for close_price, vol_rolling_day join to options base

--- dbt/models/intermediate/options_pricing/int_ohlcv__rolling_vol.sql
+++ dbt/models/intermediate/options_pricing/int_ohlcv__rolling_vol.sql
@@ -7,50 +7,76 @@
 	description='Enhanced OHLCV data with historical rolling window volatility (std dev) added per vol_window var'
 ) }}
 
-with rolling_vol as (
+with ohlcv_ranked as (
 	select
-		symbol,
 		market_date,
+		symbol,
 		close_price,
 		log_return,
 		created_date,
-
-		-- Compute rolling volatility as rolling-window standard deviation for historical volatility estimate
-		stddev(log_return) over (
-			partition by symbol
-			order by market_date
-			rows between {{ var('vol_window') - 1 }} preceding and current row
-		) as rolling_vol_daily,
-
-		-- Data quality check to determine if a full window has been used for the volatility estimate
-		count(log_return) over (
-			partition by symbol
-			order by market_date
-			rows between {{ var('vol_window') - 1 }} preceding and current row
-		) as sample_size_vol_window
+		row_number() over (
+			partition by market_date, symbol
+			order by created_date desc
+		) as rank_i
 	from {{ ref('stg_tradier__ohlcv_bars') }}
+),
+
+ohlcv_dedup as (
+	select
+		market_date,
+		symbol,
+		close_price,
+		log_return,
+		created_date
+	from ohlcv_ranked
+	where rank_i = 1
+),
+
+rolling_vol as (
+	select
+		market_date,
+		symbol,
+		close_price,
+		log_return,
+		created_date,
+		stddev_samp(log_return) over(
+			partition by symbol
+			order by market_date
+			rows between {{ var('vol_window')-1 }} preceding and current row
+		) as vol_rolling_day,
+		count(log_return) over(
+			partition by symbol
+			order by market_date
+			rows between {{ var('vol_window')-1 }} preceding and current row
+		) as vol_window_sample_size,
+	from ohlcv_dedup
 	where log_return is not null
 ),
-rolling_vol_annualized as (
+
+final_dataset as (
 	select
-		*,
-		rolling_vol_daily * sqrt({{ var('trading_days_per_annum') }}) as rolling_vol_annualized,
+		market_date,
+		symbol,
+		close_price,
+		round(log_return, 4) as log_return,
+		vol_rolling_day,
+		(vol_window_sample_size >= {{ var('vol_window')-1 }}) as vol_is_valid_window,
 		case
-			when sample_size_vol_window >= {{ var('vol_window') }}
-			then true
-			else false
-		end as is_valid_window
+			when vol_window_sample_size >= {{ var('vol_window')-1 }}
+			then vol_rolling_day * sqrt({{ var('trading_days_per_annum') }})
+			else null
+		end as vol_rolling_day_annualized,
+		created_date
 	from rolling_vol
 )
 
 select
-	symbol,
 	market_date,
+	symbol,
 	close_price,
 	log_return,
-	rolling_vol_annualized,
-	is_valid_window,
-	sample_size_vol_window,
+	vol_rolling_day,
+	vol_rolling_day_annualized,
+	vol_is_valid_window,
 	created_date
-from rolling_vol_annualized
-order by symbol, market_date
+from final_dataset
