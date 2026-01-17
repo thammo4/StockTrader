4e2cbf9c58234a7c406a530b8f6081055b56ba5b
tom hammons <thammo4@uic.edu>
2026-01-17 16:01:32 -0500

    update qlib builder functions to reflect datetime.date treatment of market_date, expiry_date

--- src/StockTrader/pricing/qlib/builders.py
+++ src/StockTrader/pricing/qlib/builders.py
@@ -2,14 +2,26 @@
 # FILE: `StockTrader/src/StockTrader/pricing/qlib/builders.py`
 #
 
+"""
+QuantLib Object Builders
+
+Provides standardized interface to implement QuantLib objects commonly employed during options pricing.
+"""
+
+# import QuantLib as ql
+# from typing import Tuple
+
+# from StockTrader.pricing.qlib.context import get_context
+
 import QuantLib as ql
+from datetime import date
 from typing import Tuple
 
 from StockTrader.pricing.qlib.context import get_context
 
-def build_payoff (option_type:str, strike: float) -> ql.PlainVanillaPayoff:
+def build_payoff (option_type:str, K: float) -> ql.PlainVanillaPayoff:
 	ql_type = ql.Option.Call if option_type.lower() == "call" else ql.Option.Put
-	return ql.PlainVanillaPayoff(ql_type, strike)
+	return ql.PlainVanillaPayoff(ql_type, K)
 
 def build_amr_exercise (eval_date: ql.Date, expiry_date: ql.Date) -> ql.AmericanExercise:
 	return ql.AmericanExercise(eval_date, expiry_date)
@@ -36,20 +48,17 @@ def build_market_date_handles (eval_date: ql.Date, S: float, r: float, q: float,
 
 	return spotH, rateH, divH, volH
 
-
 def build_bsm_process (spotH: ql.QuoteHandle, rateH: ql.YieldTermStructureHandle, divH: ql.YieldTermStructureHandle, volH: ql.BlackVolTermStructureHandle) -> ql.BlackScholesMertonProcess:
 	return ql.BlackScholesMertonProcess(spotH, divH, rateH, volH)
 
-
 def build_crr_binom_engine (process: ql.BlackScholesMertonProcess, n_steps: int) -> ql.BinomialVanillaEngine:
 	return ql.BinomialVanillaEngine(process, "crr", n_steps)
-
 
 def build_option_with_engine(option_type:str, S: float, K: float, r: float, q: float, σ: float, market_date: str, expiry_date: str, n_steps: int=225) -> Tuple[ql.VanillaOption, ql.BlackScholesMertonProcess]:
 	ctx = get_context()
 
 	eval_date = ctx.set_eval_date(market_date)
-	expr_date = ctx.parse_date(expiry_date)
+	expr_date = ctx.to_ql_date(expiry_date)
 
 	payoff = build_payoff(option_type, K)
 	exercise = build_amr_exercise(eval_date, expr_date)
@@ -63,12 +72,3 @@ def build_option_with_engine(option_type:str, S: float, K: float, r: float, q: f
 	option.setPricingEngine(engine)
 
 	return option, bsm_process
-
-
-
-
-
-
-
-
-
