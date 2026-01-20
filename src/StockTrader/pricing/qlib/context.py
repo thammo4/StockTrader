4bdc4e0faf6949359df6cb8317e8852667f88a09
tom hammons <thammo4@uic.edu>
2026-01-19 23:44:16 -0500

    apply black formatting to qlib pricing package builders and context

--- src/StockTrader/pricing/qlib/context.py
+++ src/StockTrader/pricing/qlib/context.py
@@ -7,9 +7,9 @@
 QuantLib Context Management.
 
 Functionality/Purpose:
-	1. Map string dates to QuantLib date objects (e.g. ql.Date)
-	2. Define global QuantLib evaluation dates prior to pricing
-	3. Construct shared calendar and day counter QuantLib objects
+        1. Map string dates to QuantLib date objects (e.g. ql.Date)
+        2. Define global QuantLib evaluation dates prior to pricing
+        3. Construct shared calendar and day counter QuantLib objects
 """
 
 import QuantLib as ql
@@ -18,21 +18,21 @@ from datetime import date
 
 
 class QLibContext:
-	def __init__ (self):
-		self.calendar = ql.UnitedStates(ql.UnitedStates.NYSE)
-		self.day_counter = ql.Actual365Fixed()
-		self._date_cache = {}
+    def __init__(self):
+        self.calendar = ql.UnitedStates(ql.UnitedStates.NYSE)
+        self.day_counter = ql.Actual365Fixed()
+        self._date_cache = {}
 
-	def to_ql_date (self, d: date) -> ql.Date:
-		"""Convert datetime.date  obj to QuantLib ql.Date (+cache)"""
-		if d not in self._date_cache:
-			self._date_cache[d] = ql.Date(d.day, d.month, d.year)
-		return self._date_cache[d]
+    def to_ql_date(self, d: date) -> ql.Date:
+        """Convert datetime.date  obj to QuantLib ql.Date (+cache)"""
+        if d not in self._date_cache:
+            self._date_cache[d] = ql.Date(d.day, d.month, d.year)
+        return self._date_cache[d]
 
-	def set_eval_date (self, d: date) -> ql.Date:
-		eval_date = self.to_ql_date(d)
-		ql.Settings.instance().evaluationDate = eval_date
-		return eval_date
+    def set_eval_date(self, d: date) -> ql.Date:
+        eval_date = self.to_ql_date(d)
+        ql.Settings.instance().evaluationDate = eval_date
+        return eval_date
 
 
 #
@@ -42,9 +42,9 @@ class QLibContext:
 # _context: QLibContext = None
 _context: Optional[QlibContext] = None
 
-def get_context() -> QLibContext:
-	global _context
-	if _context is None:
-		_context = QLibContext()
-	return _context
 
+def get_context() -> QLibContext:
+    global _context
+    if _context is None:
+        _context = QLibContext()
+    return _context
