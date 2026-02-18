f32119407a372d9073d5f16e09dab6a39b3cf777
tom hammons <thammo4@uic.edu>
2026-02-18 17:24:52 -0500

    update qlib builders to wrap volatility in ql.SimpleQuote and avoid reconstructing the entire bsm process during iv solver

--- src/StockTrader/pricing/qlib/builders.py
+++ src/StockTrader/pricing/qlib/builders.py
@@ -7,38 +7,22 @@ QuantLib Object Builders
 
 Provides standardized interface to implement QuantLib objects commonly employed during options pricing.
 """
-
 import QuantLib as ql
+
+from dataclasses import dataclass
 from datetime import date
 from typing import Tuple
 
 from StockTrader.pricing.qlib.context import get_context
 
-
-def build_payoff(option_type: str, K: float) -> ql.PlainVanillaPayoff:
-    ql_type = ql.Option.Call if option_type.lower() == "call" else ql.Option.Put
-    return ql.PlainVanillaPayoff(ql_type, K)
+@dataclass
+class MarketQuoteHandles:
+    spotQ: ql.SimpleQuote
+    volQ: ql.SimpleQuote
 
 
 def build_amr_exercise(eval_date: ql.Date, expiry_date: ql.Date) -> ql.AmericanExercise:
     return ql.AmericanExercise(eval_date, expiry_date)
-
-
-def build_vanilla_option(payoff: ql.PlainVanillaPayoff, exercise: ql.Exercise) -> ql.VanillaOption:
-    return ql.VanillaOption(payoff, exercise)
-
-
-# def build_market_date_handles(
-def build_market_data_handles(
-    eval_date: ql.Date, S: float, r: float, q: float, σ: float
-) -> Tuple[ql.QuoteHandle, ql.YieldTermStructureHandle, ql.YieldTermStructureHandle, ql.BlackVolTermStructureHandle]:
-    ctx = get_context()
-    spotH = ql.QuoteHandle(ql.SimpleQuote(S))
-    rateH = ql.YieldTermStructureHandle(ql.FlatForward(eval_date, r, ctx.day_counter))
-    divH = ql.YieldTermStructureHandle(ql.FlatForward(eval_date, q, ctx.day_counter))
-    volH = ql.BlackVolTermStructureHandle(ql.BlackConstantVol(eval_date, ctx.calendar, σ, ctx.day_counter))
-
-    return spotH, rateH, divH, volH
 
 
 def build_bsm_process(
@@ -54,6 +38,40 @@ def build_crr_binom_engine(process: ql.BlackScholesMertonProcess, n_steps: int) 
     return ql.BinomialVanillaEngine(process, "crr", n_steps)
 
 
+def build_market_data_handles(
+    eval_date: ql.Date,
+    S:float,
+    r: float,
+    q: float,
+    σ: float
+) -> Tuple[
+    ql.QuoteHandle,
+    ql.YieldTermStructureHandle,
+    ql.YieldTermStructureHandle,
+    ql.BlackVolTermStructureHandle,
+    MarketQuoteHandles
+]:
+    ctx = get_context()
+
+    spotQ = ql.SimpleQuote(S)
+    volQ = ql.SimpleQuote(σ)
+
+    spotH = ql.QuoteHandle(spotQ)
+
+    rateH = ql.YieldTermStructureHandle(ql.FlatForward(eval_date, r, ctx.day_counter))
+    divH = ql.YieldTermStructureHandle(ql.FlatForward(eval_date, q, ctx.day_counter))
+
+    volTS = ql.BlackConstantVol(
+        eval_date,
+        ctx.calendar,
+        ql.QuoteHandle(volQ),
+        ctx.day_counter
+    )
+    volH = ql.BlackVolTermStructureHandle(volTS)
+
+    return spotH, rateH, divH, volH, MarketQuoteHandles(spotQ=spotQ,volQ=volQ)
+
+
 def build_option_with_engine(
     option_type: str,
     S: float,
@@ -64,7 +82,11 @@ def build_option_with_engine(
     market_date: date,
     expiry_date: date,
     n_steps: int = 225,
-) -> Tuple[ql.VanillaOption, ql.BlackScholesMertonProcess]:
+) -> Tuple[
+    ql.VanillaOption,
+    # ql.BlackScholesMertonProcess,
+    MarketQuoteHandles
+]:
     ctx = get_context()
 
     eval_date = ctx.set_eval_date(market_date)
@@ -74,11 +96,21 @@ def build_option_with_engine(
     exercise = build_amr_exercise(eval_date, expr_date)
     option = build_vanilla_option(payoff, exercise)
 
-    spotH, rateH, divH, volH = build_market_data_handles(eval_date, S, r, q, σ)
+    spotH, rateH, divH, volH, market_quote_handles = build_market_data_handles(eval_date, S, r, q, σ)
 
     bsm_process = build_bsm_process(spotH, rateH, divH, volH)
 
     engine = build_crr_binom_engine(bsm_process, n_steps)
     option.setPricingEngine(engine)
 
-    return option, bsm_process
+    # return option, bsm_process, market_quote_handles
+    return option, market_quote_handles
+
+
+def build_payoff(option_type: str, K: float) -> ql.PlainVanillaPayoff:
+    ql_type = ql.Option.Call if option_type.lower() == "call" else ql.Option.Put
+    return ql.PlainVanillaPayoff(ql_type, K)
+
+
+def build_vanilla_option(payoff: ql.PlainVanillaPayoff, exercise: ql.Exercise) -> ql.VanillaOption:
+    return ql.VanillaOption(payoff, exercise)
