4fc9b566590a4f5e5ba09f8bb65963031b6bbab6
tom hammons <thammo4@uic.edu>
2026-02-18 21:19:36 -0500

    rewrite CRRBinomialAmericanDividends to leverage quantlib observer pattern and increase efficiency of implied volatility calculations

--- src/StockTrader/pricing/qlib/models/crr_bopm_amr_divs.py
+++ src/StockTrader/pricing/qlib/models/crr_bopm_amr_divs.py
@@ -23,18 +23,74 @@ class CRRBinomialAmericanDividends (BasePricingModel):
 		self.iv_config: BrentIVConfig=kwargs.get("iv_config", BrentIVConfig())
 
 	def _build (self, option:OptionRow):
-		opt, _ = build_option_with_engine(
-			option_type=option.option_type,
-			S=option.S,
-			K=option.K,
-			r=option.r,
-			q=option.q,
-			σ=option.σ,
-			market_date=option.market_date,
-			expiry_date=option.expiry_date,
-			n_steps=option.n_steps
+		return build_option_with_engine(
+			option_type = option.option_type,
+			S = option.S,
+			K = option.K,
+			r = option.r,
+			q = option.q,
+			σ = option.σ,
+			market_date = option.market_date,
+			expiry_date = option.expiry_date,
+			n_steps = self.n_steps
 		)
-		return opt
 
 	def price (self, option:OptionRow) -> PricingResult:
-		pass
+		pricing_result = PricingResult(
+			occ=option.occ,
+			model_name=self.name,
+			n_steps = self.n_steps
+		)
+
+		t0 = time.time()
+
+		#
+		# NPV
+		#
+
+		try:
+			ql_option, market_quote_handles = self._build(option)
+			npv = ql_option.NPV()
+			if not math.isfinite(npv):
+				raise NPVCalculationError(message=f"Inf npv: {npv}", occ=option.occ)
+			pricing_result.npv = npv
+		except Exception as e:
+			pricing_result.npv_err = f"{type(e).__name__}: {str(e)[:100]}"
+			pricing_result.compute_ms = (time.time()-t0)*1000
+			return pricing_result
+
+		#
+		# Greeks - Δ, Γ, Θ
+		#
+
+		try:
+			pricing_result.Δ = ql_option.delta()
+			pricing_result.Γ = ql_option.gamma()
+			pricing_result.Θ = ql_option.theta()
+		except Exception as e:
+			pricing_result.greek_err = f"{type(e).__name__}: {str(e)[:100]}"
+
+
+		#
+		# Implied Volatility
+		#
+
+		def f_reprice (σ: float) -> float:
+			market_quote_handles.volQ.setValue(σ)
+			return ql_option.NPV()
+
+		σ_iv, σ_iv_err, _ = try_solve_implied_vol_brent(
+			f_reprice,
+			option.p_m,
+			cfg=self.iv_config,
+			occ=option.occ,
+			σ_guess=option.σ
+		)
+		pricing_result.σ_iv = σ_iv
+		pricing_result.σ_iv_err = σ_iv_err
+
+		market_quote_handles.volQ.setValue(option.σ)
+
+		pricing_result.compute_ms = (time.time() - t0) * 1000
+
+		return pricing_result
