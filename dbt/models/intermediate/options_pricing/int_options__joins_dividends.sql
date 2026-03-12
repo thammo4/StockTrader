956600189a057846766bb3fc83ce5b62f6691d2d
tom hammons <thammo4@uic.edu>
2026-03-12 00:47:19 -0400

    rewrite/update int_options__joins_dividends to consume new upstream int_dividends__maps_to_daily instead of previous int_dim__daily_dividends

--- dbt/models/intermediate/options_pricing/int_options__joins_dividends.sql
+++ dbt/models/intermediate/options_pricing/int_options__joins_dividends.sql
@@ -38,41 +38,77 @@ dividends as (
 	select
 		market_date,
 		symbol,
-		dividend_yield_annualized,
-		dividend_yield_ttm,
-		prev_ex_dividend_date as dividend_date_ref,
+		prev_ex_dividend_date,
+		prev_cash_amount,
+		prev_frequency,
+		dividend_cash_ttm,
+		is_complete_dividend_history
+	from {{ ref('int_dividends__maps_to_daily') }}
+),
+
+joined_options_dividends as (
+	select
+		o.market_date,
+		o.symbol,
+		o.occ,
+		o.option_type,
+		o.expiry_date,
+		o.ttm_days,
+		o.strike_price,
+		o.bid_price,
+		o.ask_price,
+		o.mid_price,
+		o.bid_ask_spread,
+		o.volume,
+		o.open_interest,
+		o.bid_size,
+		o.ask_size,
+		o.spot_price,
+		o.sigma,
+		o.risk_free_rate,
+		o.rate_date_ref,
+
+		d.prev_ex_dividend_date as dividend_date_ref,
+		d.is_complete_dividend_history,
+		d.dividend_cash_ttm / o.spot_price as dividend_yield_ttm,
+		
 		case
-			when prev_ex_dividend_date is null then 'no_dividend_history'
-			when dividend_yield_annualized is null then 'no_yield_calc'
-			when dividend_yield_annualized = 0.0 then 'zero_yield'
-			else 'yes_dividend'
+			when not d.is_complete_dividend_history then 0.0
+			when d.prev_frequency is null then null
+			else (d.prev_cash_amount*d.prev_frequency)/o.spot_price
+		end as dividend_yield_annualized,
+
+		case
+			when d.is_complete_dividend_history = true then 'yes_dividend'
+			else 'no_dividend'
 		end as dividend_status
-	from {{ ref('int_dim__daily_dividends') }}
+	from options_base o
+	join dividends d using (market_date, symbol)
 )
 
 select
-	o.market_date,
-	o.symbol,
-	o.occ,
-	o.option_type,
-	o.expiry_date,
-	o.ttm_days,
-	o.strike_price,
-	o.bid_price,
-	o.ask_price,
-	o.mid_price,
-	o.bid_ask_spread,
-	o.volume,
-	o.open_interest,
-	o.bid_size,
-	o.ask_size,
-	o.spot_price,
-	o.sigma,
-	o.risk_free_rate,
-	o.rate_date_ref,
-	d.dividend_yield_annualized,
-	d.dividend_yield_ttm,
-	d.dividend_date_ref,
-	d.dividend_status
-from options_base o
-left join dividends d using (market_date, symbol)
+	market_date,
+	symbol,
+	occ,
+	option_type,
+	expiry_date,
+	ttm_days,
+	strike_price,
+	bid_price,
+	ask_price,
+	mid_price,
+	bid_ask_spread,
+	volume,
+	open_interest,
+	bid_size,
+	ask_size,
+	spot_price,
+	sigma,
+	risk_free_rate,
+	rate_date_ref,
+	dividend_yield_ttm,
+	dividend_yield_annualized,
+	dividend_status,
+	is_complete_dividend_history,
+	dividend_date_ref
+from joined_options_dividends
