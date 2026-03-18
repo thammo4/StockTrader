09f9c935e960a05243885d7776a6c827e2f55182
tom hammons <thammo4@uic.edu>
2026-03-17 23:40:15 -0400

    remove unnecessary deduplication CTEs from int_ohlcv__rolling_vol

--- dbt/models/intermediate/options_pricing/int_ohlcv__rolling_vol.sql
+++ dbt/models/intermediate/options_pricing/int_ohlcv__rolling_vol.sql
@@ -7,67 +7,48 @@
 	description='Enhanced OHLCV data with historical rolling window volatility (std dev) added per vol_window var'
 ) }}
 
-with ohlcv_ranked as (
+with ohlcv_bars_base as (
 	select
 		market_date,
 		symbol,
 		close_price,
-		log_return,
-		created_date,
-		row_number() over (
-			partition by market_date, symbol
-			order by created_date desc
-		) as rank_i
+		log_return
 	from {{ ref('stg_tradier__ohlcv_bars') }}
+	where log_return is not null
 ),
 
-ohlcv_dedup as (
+calc_rolling_vol as (
 	select
 		market_date,
 		symbol,
 		close_price,
 		log_return,
-		created_date
-	from ohlcv_ranked
-	where rank_i = 1
-),
-
-rolling_vol as (
-	select
-		market_date,
-		symbol,
-		close_price,
-		log_return,
-		created_date,
-		stddev_samp(log_return) over(
+		stddev_samp(log_return) over (
 			partition by symbol
 			order by market_date
 			rows between {{ var('vol_window')-1 }} preceding and current row
 		) as vol_rolling_day,
-		count(log_return) over(
+		count(log_return) over (
 			partition by symbol
 			order by market_date
 			rows between {{ var('vol_window')-1 }} preceding and current row
-		) as vol_window_sample_size,
-	from ohlcv_dedup
-	where log_return is not null
+		) as vol_window_sample_size
+	from ohlcv_bars_base
 ),
-
-final_dataset as (
+calc_rolling_vol_annualized as (
 	select
 		market_date,
 		symbol,
 		close_price,
 		round(log_return, 4) as log_return,
 		vol_rolling_day,
-		(vol_window_sample_size >= {{ var('vol_window')-1 }}) as vol_is_valid_window,
+		vol_window_sample_size >= {{ var('vol_window')-1 }} as vol_is_valid_window,
 		case
 			when vol_window_sample_size >= {{ var('vol_window')-1 }}
 			then vol_rolling_day * sqrt({{ var('trading_days_per_annum') }})
 			else null
-		end as vol_rolling_day_annualized,
-		created_date
-	from rolling_vol
+		end as vol_rolling_day_annualized
+	from calc_rolling_vol
 )
 
 select
@@ -77,6 +58,5 @@ select
 	log_return,
 	vol_rolling_day,
 	vol_rolling_day_annualized,
-	vol_is_valid_window,
-	created_date
-from final_dataset
+	vol_is_valid_window
+from calc_rolling_vol_annualized
