66a177f764265621234f082cf3a898d21ddf8183
thammo4 <thammo4@uic.edu>
2026-03-19 00:31:35 -0500

    update ingest_tradier_dividends to implement atomic writes

--- scripts/ingest_tradier_dividends.py
+++ scripts/ingest_tradier_dividends.py
@@ -6,25 +6,18 @@ import os
 import pandas as pd
 from StockTrader.settings import STOCK_TRADER_DWH, logger, today, today_eighteen_months_ago
 from utils.dividend_table import dividend_table
+from utils.write_atomic import write_parquet_atomic
 from airflow.exceptions import AirflowSkipException
 
 
-def get_symbols():
-    path = "/opt/stocktrader/largecap_all.txt"
-    if os.path.exists(path):
-        return pd.read_table(path, header=None)[0].to_list()
-    else:
-        logger.warning(f"No symbol list: {path}")
-        return []
-
-
 def ingest_tradier_dividends(symbol, subdir="dividends_af"):
+    logger.info(f"Running dividend ingest, symbol={symbol} [ingest_tradier_dividends]")
     try:
         df = dividend_table(symbol=symbol, start_date=today_eighteen_months_ago)
 
         if df is None or df.empty:
-            logger.warning(f"No dividend data, symbol={symbol}")
-            raise AirflowSkipException(f"No dividend data, symbol={symbol}")
+            logger.warning(f"No dividend data, symbol={symbol} [ingest_tradier_dividends]")
+            raise AirflowSkipException(f"No dividend data, symbol={symbol} [ingest_tradier_dividends]")
 
         df["symbol"] = symbol
         df["created_date"] = today
@@ -37,10 +30,10 @@ def ingest_tradier_dividends(symbol, subdir="dividends_af"):
         os.makedirs(dir_landing, exist_ok=True)
         fpath_parquet = os.path.join(dir_landing, f"{symbol}.parquet")
 
-        df.to_parquet(fpath_parquet, index=False, engine="pyarrow")
-        logger.info(f"Wrote dividend data, symbol={symbol} [{len(df)} records]")
+        write_parquet_atomic(df, fpath_parquet)
+        logger.info(f"Wrote dividend data, symbol={symbol}, n={len(df)} records [ingest_tradier_dividends]")
     except AirflowSkipException:
         raise
     except Exception as e:
-        logger.error(f"Failed dividend ingest, sybmol={symbol}: {str(e)}")
+        logger.error(f"Failed dividend ingest, symbol={symbol}: {str(e)} [ingest_tradier_dividends]")
         raise
