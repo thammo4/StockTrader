45288ef6652134864550f2da27f67fcd390b607e
thammo4 <thammo4@uic.edu>
2026-03-19 14:26:49 -0500

    revise ingest_tradier_options error handling to ensure AirflowSkipException gets triggered for any bad symbol/invalid api response/etc

--- scripts/ingest_tradier_options.py
+++ scripts/ingest_tradier_options.py
@@ -6,22 +6,13 @@ import os
 import pandas as pd
 from StockTrader.tradier import options_dataL
 from StockTrader.settings import STOCK_TRADER_DWH, logger, today
+from utils.write_atomic import write_parquet_atomic
 from airflow.exceptions import AirflowSkipException
-
-
-def get_symbols():
-    path = "/opt/stocktrader/largecap_all.txt"
-    if os.path.exists(path):
-        return pd.read_table(path, header=None)[0].to_list()
-    else:
-        logger.warning(f"No symbol list: {path}")
-        return []
 
 
 #
 # Retrieve + Store today's options chain from Tradier for a given ticker symbol
 #
-
 
 def ingest_tradier_options(symbol, subdir="options_af"):
     try:
@@ -30,9 +21,9 @@ def ingest_tradier_options(symbol, subdir="options_af"):
         #
 
         df = options_dataL.get_chain_all(symbol=symbol)
-        if df.empty:
-            logger.warning(f"No options data: {symbol}")
-            return
+        if df is None or df.empty:
+            logger.info(f"No options data, symbol={symbol} [ingest_tradier_options]")
+            raise AirflowSkipException(f"No options data, symbol={symbol} [ingest_tradier_options]")
         df["created_date"] = today
 
         #
@@ -51,11 +42,18 @@ def ingest_tradier_options(symbol, subdir="options_af"):
             df_existing = pd.read_parquet(fpath_parquet)
             df = pd.concat([df_existing, df]).drop_duplicates(subset=["symbol", "created_date"])
 
-        df.to_parquet(fpath_parquet, index=False, engine="pyarrow")
-        logger.info(f"Loaded options chain, symbol={symbol}")
+        write_parquet_atomic(df, fpath_parquet)
+        logger.info(f"Loaded options chain, symbol={symbol} [ingest_tradier_options]")
 
     except AirflowSkipException:
         raise
+
+    #
+    # Exception defined to catch bad/delisted symbols per uvatradier `get_chain_day` error handling
+    #
+
     except Exception as e:
-        logger.error(f"Ingest failed, symbol={symbol}: {str(e)}")
-        raise
+        # logger.error(f"Ingest failed, symbol={symbol}: {str(e)} [ingest_tradier_options]")
+        # raise
+        logger.warning(f"Skipping symbol={symbol}: {str(e)} [ingest_tradier_options]")
+        raise AirflowSkipException(f"No data, symbol={symbol}: {str(e)}")
