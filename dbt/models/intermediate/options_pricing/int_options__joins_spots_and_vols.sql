4d1042545dd8b74a7a5455cf353a9dc7f0171336
tom hammons <thammo4@uic.edu>
2026-04-02 19:48:30 -0400

    update int_options__joins_spots_and_vols dbt options pricing model to consume int_ohlcv__calcs_rolling_vols appropriate historical sigma value

--- dbt/models/intermediate/options_pricing/int_options__joins_spots_and_vols.sql
+++ dbt/models/intermediate/options_pricing/int_options__joins_spots_and_vols.sql
@@ -7,6 +7,7 @@
 	incremental_strategy='append'
 ) }}
 
+
 with options_base as (
 	select
 		market_date,
@@ -16,12 +17,12 @@ with options_base as (
 		expiry_date,
 		ttm_days,
 		strike_price,
+		mid_price,
 		bid_price,
 		ask_price,
-		mid_price,
 		bid_ask_spread,
-		volume,
-		open_interest,
+		volume::int as volume,
+		open_interest::int as open_interest,
 		bid_size,
 		ask_size
 	from {{ ref('int_options__creates_base_dset') }}
@@ -30,36 +31,83 @@ with options_base as (
 	{% endif %}
 ),
 
-underlying_price_and_rolling_vol as (
+underlying_vols as (
 	select
 		market_date,
 		symbol,
-		close_price as spot_price,
-		vol_rolling_day_annualized as sigma,
-		vol_is_valid_window as sigma_is_valid
-	from {{ ref('int_ohlcv__rolling_vol') }}
-	where vol_rolling_day_annualized is not null
+		sigma_5d,
+		sigma_21d,
+		sigma_42d,
+		sigma_63d,
+		sigma_252d
+	from {{ ref('int_ohlcv__calcs_rolling_vols') }}
+),
+
+underlying_spots as (
+	select
+		market_date,
+		symbol,
+		close_price as spot_price
+	from {{ ref('stg_tradier__ohlcv_bars') }}
+),
+
+joined_options_underlyings as (
+	select
+		o.market_date,
+		o.symbol,
+		o.occ,
+		o.option_type,
+		o.expiry_date,
+		o.ttm_days,
+		o.strike_price,
+		o.mid_price,
+		o.bid_price,
+		o.ask_price,
+		o.bid_ask_spread,
+		o.volume,
+		o.open_interest,
+		o.bid_size,
+		o.ask_size,
+		us.spot_price,
+		case
+			when o.ttm_days between 1 and 7 then uv.sigma_5d
+			when o.ttm_days between 8 and 30 then uv.sigma_21d
+			when o.ttm_days between 31 and 60 then uv.sigma_42d
+			when o.ttm_days between 61 and 120 then uv.sigma_63d
+			else uv.sigma_252d
+		end as sigma,
+		uv.sigma_5d,
+		uv.sigma_21d,
+		uv.sigma_42d,
+		uv.sigma_63d,
+		uv.sigma_252d
+	from options_base o
+	join underlying_spots us using (market_date, symbol)
+	join underlying_vols uv using (market_date, symbol)
 )
 
 select
-	o.market_date,
-	o.symbol,
-	o.occ,
-	o.option_type,
-	o.expiry_date,
-	o.ttm_days,
-	o.strike_price,
-	o.bid_price,
-	o.ask_price,
-	o.mid_price,
-	o.bid_ask_spread,
-	o.volume,
-	o.open_interest,
-	o.bid_size,
-	o.ask_size,
-
-	u.spot_price,
-	u.sigma,
-	u.sigma_is_valid
-from options_base o
-join underlying_price_and_rolling_vol u using (symbol, market_date)
+	market_date,
+	symbol,
+	occ,
+	option_type,
+	expiry_date,
+	ttm_days,
+	strike_price,
+	mid_price,
+	bid_price,
+	ask_price,
+	bid_ask_spread,
+	volume,
+	open_interest,
+	bid_size,
+	ask_size,
+	spot_price,
+	sigma,
+	sigma_5d,
+	sigma_21d,
+	sigma_42d,
+	sigma_63d,
+	sigma_252d
+from joined_options_underlyings
+where sigma is not null
