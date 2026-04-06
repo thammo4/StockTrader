753273f72af74852510c7051e13607ff97d76f87
tom hammons <thammo4@uic.edu>
2026-04-05 20:47:39 -0400

    rewrite mart_vrp__high_ivp_candidates to 1. filter out LEAPS and deep_itm/otm contracts prior to computing iv CDF; 2. apply tradability filters to produce succinct tradable subset

--- dbt/models/marts/priced_options/mart_vrp__high_ivp_candidates.sql
+++ dbt/models/marts/priced_options/mart_vrp__high_ivp_candidates.sql
@@ -4,29 +4,150 @@
 
 {{ config(materialized='view') }}
 
-{% set ivp_threshold = 0.80 %}
-{% set n_partition_min = 30 %}
-{% set min_volume = 10 %}
-{% set min_open_interest = 100 %}
-{% set max_bid_ask_spread_pct = 0.10 %}
+
+--
+-- Trading Activity Cutoffs
+--
+
+{% set min_volume = 12 %}
+{% set min_open_interest = 105 %}
+{% set min_bid_size = 12 %}
+{% set min_ask_size = 12 %}
+{% set min_time_value_bid_price = 0 %}
+{% set max_bid_ask_spread_pct = 0.09 %}
 
 
-with source as (select * from {{ ref('int_options__calcs_ivp') }}),
+--
+-- IV Cutoffs
+--
 
-ok_partition as (select * from source where iv_partition_n >= {{ n_partition_min }}),
+{% set min_vrp_ratio = 1.095 %}
+{% set min_vrp_spread = 0.095 %}
+{% set min_iv_cdf = 0.8925 %}
+{% set min_iv_partition_n = 30 %}
 
-ok_tradable as (
-	select *
-	from ok_partition
-	where volume >= {{ min_volume }}
-	and open_interest >= {{ min_open_interest }}
-	and (ask_price - bid_price) / mid_price <= {{ max_bid_ask_spread_pct }}
+
+--
+-- Moneyness Cutoffs
+--
+
+{% set min_abs_delta = 0.455 %}
+{% set max_abs_delta = 0.545 %}
+
+
+--
+-- Symbol Count Cutoffs
+--
+
+{% set max_symbol_rank = 2 %}
+
+
+with ivp_population as (
+	select
+		*
+	from {{ ref('int_options__calcs_vrp') }}
+	where dte_bucket != 'leaps'
+	and moneyness_category not in ('deep_itm', 'deep_otm')
 ),
 
-high_ivp as (
-	select *
-	from ok_tradable
-	where iv_cdf >= {{ ivp_threshold }}
+ivp as (
+	select
+		*,
+		cume_dist() over (partition by symbol, option_type, moneyness_category, dte_bucket order by iv) as iv_cdf,
+		count(*) over (partition by symbol, option_type, moneyness_category, dte_bucket) as iv_partition_n
+	from ivp_population
+),
+
+todays_contracts as (
+	select
+		*
+	from ivp
+	where market_date = (select max(market_date) from ivp)
+	and expiry_date > current_date
+),
+
+tradable_cutoffs as (
+	select
+		*
+	from todays_contracts
+	where volume > {{ min_volume }}
+	and open_interest > {{ min_open_interest }}
+	and bid_size > {{ min_bid_size }}
+	and ask_size > {{ min_ask_size }}
+	and (ask_price-bid_price)/nullif(bid_price,0) < {{ max_bid_ask_spread_pct }}
+	and time_value_bid_price > 0
+),
+
+iv_cutoffs as (
+	select
+		*
+	from tradable_cutoffs
+	where vrp_ratio > {{ min_vrp_ratio }}
+	and vrp_spread > {{ min_vrp_spread }}
+	and iv_cdf > {{ min_iv_cdf }}
+	and iv_partition_n > {{ min_iv_partition_n }}
+),
+
+delta_cutoffs as (
+	select
+		*
+	from iv_cutoffs
+	where abs(delta) between {{ min_abs_delta }} and {{ max_abs_delta }}
+),
+
+symbol_count_cutoffs as (
+	select
+		*,
+		row_number() over (partition by symbol order by iv_cdf desc, vrp_spread desc) as symbol_rank
+	from delta_cutoffs
+	qualify symbol_rank <= {{ max_symbol_rank }}
 )
 
-select * from high_ivp
+select
+	market_date,
+	symbol,
+	option_type,
+	moneyness_category as mnys,
+	dte_bucket,
+	occ,
+	expiry_date,
+	ttm_days as dte,
+	strike_price,
+	spot_price,
+	mid_price,
+	bid_price,
+	ask_price,
+	intrinsic_price,
+	time_value_mid_price as tv_mid,
+	time_value_bid_price as tv_bid,
+	time_value_ask_price as tv_ask,
+	volume,
+	open_interest as oi,
+	bid_size,
+	ask_size,
+	round(sigma,4) as sigma,
+	risk_free_rate,
+	round(dividend_yield_annualized,4) as div_yield_ann,
+	moneyness_ratio as mnys_ratio,
+	moneyness_ratio_log as mnys_log,
+	moneyness_standardized mnys_std,
+	round(npv,2) as npv,
+	round(delta,4) as delta,
+	round(gamma,4) as gamma,
+	round(theta,4) as theta,
+	round(iv,4) as iv,
+	round(vrp_spread,4) as vrp_spread,
+	round(vrp_ratio,4) as vrp_ratio,
+	round(spread_price,2) as spread_price,
+	round(xaction_price,2) as xaction_price,
+	round(credit_price,2) as credit_price,
+	round(iv_cdf,4) as iv_cdf,
+	iv_partition_n,
+	symbol_rank
+from symbol_count_cutoffs
+order by
+	symbol,
+	option_type,
+	moneyness_category,
+	dte_bucket,
+	iv_cdf desc
