264104f6c39f55e1e3bc3604b41969235272b741
thammo4 <thammo4@uic.edu>
2026-07-07 21:56:01 -0400

    add 52 weekly frequency level to stg_tradier__dividends and redefines is_regular_dividend to include cash_amount > 0

--- dbt/models/staging/tradier/stg_tradier__dividends.sql
+++ dbt/models/staging/tradier/stg_tradier__dividends.sql
@@ -17,9 +17,11 @@ select
 		when frequency = 2 then 'semi_annual'
 		when frequency = 4 then 'quarterly'
 		when frequency = 12 then 'monthly'
+		when frequency = 52 then 'weekly'
 		else 'unknown'
 	end as dividend_type,
-	frequency > 0 and frequency < 13 as is_regular_dividend,
+	-- frequency > 0 and frequency < 13 as is_regular_dividend,
+	frequency in (0, 1, 2, 4, 12, 52) and cash_amount > 0 as is_regular_dividend,
 	created_date::date as created_date
 from source
 order by symbol, ex_dividend_date
