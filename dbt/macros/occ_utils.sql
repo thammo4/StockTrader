75803e9f7236669ccbea41f8adb8e920c0223309
thammo4 <thammo4@uic.edu>
2026-07-08 20:29:25 -0400

    add occ_utils macros for parsing full occ into symbol, expiry_date, option_type, strike_price

--- dbt/macros/occ_utils.sql
+++ dbt/macros/occ_utils.sql
@@ -2,6 +2,47 @@
 -- FILE: `StockTrader/dbt/macros/occ_utils.sql`
 --
 
+{% macro occ_pattern() -%}
+'^([A-Z0-9]{1,6})(\d{2})(\d{2})(\d{2})([CP])(\d{8})$'
+{%- endmacro %}
+
+{% macro occ_normalized(occ) -%}
+upper(trim({{ occ }}))
+{%- endmacro %}
+
+{% macro occ_parse_expiry_date(occ) -%}
+	case
+		when regexp_matches({{ occ_normalized(occ) }}, {{ occ_pattern() }})
+		then cast(
+			'20' || regexp_extract({{ occ_normalized(occ) }}, {{ occ_pattern() }}, 2)
+			|| '-' || regexp_extract({{ occ_normalized(occ) }}, {{ occ_pattern() }}, 3)
+			|| '-' || regexp_extract({{ occ_normalized(occ) }}, {{ occ_pattern() }}, 4)
+			as date
+		)
+		else null
+	end
+{%- endmacro %}
+
+{% macro occ_parse_strike(occ) -%}
+	case
+		when regexp_matches({{ occ_normalized(occ) }}, {{ occ_pattern() }})
+		then regexp_extract({{ occ_normalized(occ) }}, {{ occ_pattern() }}, 6)::bigint / 1000.0
+		else null
+	end
+{%- endmacro %}
+
+{% macro occ_parse_option_type(occ) -%}
+	case
+		when regexp_matches({{ occ_normalized(occ) }}, {{ occ_pattern() }})
+		then
+			case regexp_extract({{ occ_normalized(occ) }}, {{ occ_pattern() }}, 5)
+				when 'C' then 'call'
+				when 'P' then 'put'
+			end
+		else null
+	end
+{%- endmacro %}
+
 
 --
 -- occ_parse_underlying
@@ -11,10 +52,8 @@
 
 {% macro occ_parse_underlying(occ) -%}
 	case
-		when regexp_matches(upper(trim({{ occ }})),
-			'^([A-Z0-9]{1,6})(\d{2})(\d{2})(\d{2})([CP])(\d{8})$')
-		then regexp_extract(upper(trim({{ occ }})),
-			'^([A-Z0-9]{1,6})(\d{2})(\d{2})(\d{2})([CP])(\d{8})$', 1)
+		when regexp_matches({{ occ_normalized(occ) }}, {{ occ_pattern() }})
+		then regexp_extract({{ occ_normalized(occ) }}, {{ occ_pattern() }}, 1)
 		else null
 	end
 {%- endmacro %}
