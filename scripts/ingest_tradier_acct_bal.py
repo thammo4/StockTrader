29bf4ccf5f8df51ede430c2474b9254e2fb3469d
tom hammons <thammo4@uic.edu>
2026-08-02 23:54:12 -0700

    apply black formatting to ingest_tradier_acct and ingest_tradier_acct_bal_dag

--- scripts/ingest_tradier_acct_bal.py
+++ scripts/ingest_tradier_acct_bal.py
@@ -10,55 +10,56 @@ from StockTrader.settings import STOCK_TRADER_DWH, logger, today
 from utils.write_atomic import write_parquet_atomic
 from airflow.exceptions import AirflowSkipException
 
+
 def ingest_tradier_acct_bal(live: bool = False):
-	logger.info("Starting acct bal ingest [ingest_tradier_acct_bal]")
+    logger.info("Starting acct bal ingest [ingest_tradier_acct_bal]")
 
-	try:
-		acct_client = acctL if live else acct
- 
-		df = acct_client.get_account_balance()
-		if df is None or df.empty:
-			logger.warning("No acct bal data [ingest_tradier_acct_bal]")
-			return
+    try:
+        acct_client = acctL if live else acct
 
-		acct_id = df["account_number"].iloc[0]
-		df["created_date"] = today
-		df["created_ts"] = datetime.now().strftime("%H:%M:%S")
+        df = acct_client.get_account_balance()
+        if df is None or df.empty:
+            logger.warning("No acct bal data [ingest_tradier_acct_bal]")
+            return
 
-		#
-		# Prepare landing dir + filepath
-		#
+        acct_id = df["account_number"].iloc[0]
+        df["created_date"] = today
+        df["created_ts"] = datetime.now().strftime("%H:%M:%S")
 
-		# dir_landing = os.path.join(STOCK_TRADER_DWH, "acct_bal")
-		dir_landing = os.path.join(STOCK_TRADER_DWH, "account_af")
-		os.makedirs(dir_landing, exist_ok=True)
-		fpath_parquet = os.path.join(dir_landing, f"{acct_id}.parquet")
+        #
+        # Prepare landing dir + filepath
+        #
 
-		#
-		# Create (if not exists) or Append (if exists)
-		#
+        # dir_landing = os.path.join(STOCK_TRADER_DWH, "acct_bal")
+        dir_landing = os.path.join(STOCK_TRADER_DWH, "account_af")
+        os.makedirs(dir_landing, exist_ok=True)
+        fpath_parquet = os.path.join(dir_landing, f"{acct_id}.parquet")
 
-		if os.path.exists(fpath_parquet):
-			df_existing = pd.read_parquet(fpath_parquet)
-			df = pd.concat([df_existing, df], ignore_index=True)
-			logger.info(f"Appending n_bals={len(df_existing)} to acct={acct_id} [ingest_tradier_acct_bal]")
-		else:
-			logger.info(f"Creating bal file, acct={acct_id} [ingest_tradier_acct_bal]")
+        #
+        # Create (if not exists) or Append (if exists)
+        #
 
-		#
-		# Drop Duplicates (uncomment to enforce)
-		# - As of script inception, duplicates are ok because:
-		# 		A. Low risk of unbounded growth in data volume (few accounts, 25 cols with compressible values, etc)
-		# 		B. Account balance values change continuously throughout day, and capturing intraday changes may be useful
-		#
+        if os.path.exists(fpath_parquet):
+            df_existing = pd.read_parquet(fpath_parquet)
+            df = pd.concat([df_existing, df], ignore_index=True)
+            logger.info(f"Appending n_bals={len(df_existing)} to acct={acct_id} [ingest_tradier_acct_bal]")
+        else:
+            logger.info(f"Creating bal file, acct={acct_id} [ingest_tradier_acct_bal]")
 
-		# df.drop_duplicates(subset=["account_number", "created_date"], keep="last", inplace=True)
+        #
+        # Drop Duplicates (uncomment to enforce)
+        # - As of script inception, duplicates are ok because:
+        # 		A. Low risk of unbounded growth in data volume (few accounts, 25 cols with compressible values, etc)
+        # 		B. Account balance values change continuously throughout day, and capturing intraday changes may be useful
+        #
 
-		write_parquet_atomic(df, fpath_parquet)
-		logger.info(f"Acct bal ingest ok, acct={acct_id}, n={len(df)} [ingest_tradier_acct_bal]")
+        # df.drop_duplicates(subset=["account_number", "created_date"], keep="last", inplace=True)
 
-	except AirflowSkipException:
-		raise
-	except Exception as e:
-		logger.error(f"Acct bal ingest fail, acct={acct_id}: {str(e)} [ingest_tradier_acct_bal]")
-		raise
+        write_parquet_atomic(df, fpath_parquet)
+        logger.info(f"Acct bal ingest ok, acct={acct_id}, n={len(df)} [ingest_tradier_acct_bal]")
+
+    except AirflowSkipException:
+        raise
+    except Exception as e:
+        logger.error(f"Acct bal ingest fail, acct={acct_id}: {str(e)} [ingest_tradier_acct_bal]")
+        raise
